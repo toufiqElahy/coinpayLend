03c56903015785a2e174e1a21083dcee9d370db2
toufiqelahy@hotmail.com <toufiqelahy@hotmail.com>
2020-10-19 22:27:07 +0600

    updated

--- EthMLM/Controllers/LendController.cs
+++ EthMLM/Controllers/LendController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.NodeServices;
 using Nethereum.Web3;
+using Newtonsoft.Json.Linq;
 
 namespace EthMLM.Controllers
 {
@@ -26,7 +27,7 @@ namespace EthMLM.Controllers
             return View(userWallet);
         }
         [HttpPost]
-        public IActionResult Index(string coin,decimal amnt)
+        public IActionResult Index(string coin,double amnt)
         {
             string email = User.Identity.Name;
             var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == email);
@@ -44,44 +45,20 @@ namespace EthMLM.Controllers
             }
             return View(userWallet);
         }
-        public IActionResult Loan()
-        {
-            return View(LotteryModel._tickets);
-        }
-        public IActionResult LoanHistory()
-        {
-            return View(LotteryModel._tickets);
-        }
-        public IActionResult BuyTicket(int number)
-        {
-            var ticket= LotteryModel._tickets.First(x => x.Number == number);
-            if (ticket.IsLocked == false &&ticket.Available>0)
-            {
-                //deduct SCC coin from user account
-                LotteryModel._collectedFund += 5;//if ticket price 5 SCC
-
-                ticket.Users += User.Identity.Name + ",";
-                ticket.Available = ticket.Available - 1;
-                var userTicket = LotteryModel._userTicket.FirstOrDefault(x=>x.Email==User.Identity.Name&&x.TicketNumber==number);
-                if (userTicket == null)
-                {
-                    LotteryModel._userTicket.Add(new UserTicket { Email=User.Identity.Name,TicketNumber=number});
-                }
-                else
-                {
-                    userTicket.TicketCount++;
-                }
-                LotteryModel.LockUnlock(ticket);
-            }
-            return RedirectToAction("Index");
-        }
+        
         public IActionResult BorrowerForm()
         {
-            return View();
+            string email = User.Identity.Name;
+            var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == email);
+            return View(userWallet);
         }
         [HttpPost]
         public IActionResult BorrowerForm(Loan mloan)
         {
+            var irst = mloan.days * LoanModel._dailyInterestRate;
+            mloan.iRate = 100 * irst;
+            mloan.rPay = mloan.usd + (mloan.usd*irst);
+
             string email = User.Identity.Name;
             mloan.Email = email;
             var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == email);
@@ -168,7 +145,7 @@ namespace EthMLM.Controllers
         public IActionResult LendUSD()
         {
             //return View(LoanModel._loans.Where(x => x.status == "Pending").ToList());
-            return View(LoanModel._loans.Where(x => x.status == "Pending"&&x.Email!=User.Identity.Name).ToList());
+            return View(LoanModel._loans.Where(x => x.status == "Pending" &&x.Email!=User.Identity.Name).ToList());
         }
         public IActionResult Lend(DateTime date)
         {
@@ -202,18 +179,28 @@ namespace EthMLM.Controllers
         public IActionResult FailToRepay(DateTime date)
         {
             var mloan = LoanModel._loans.FirstOrDefault(x => x.date.ToString().Contains(date.ToString()));
+            var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == mloan.Email);
+            var lenerWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == mloan.LenderEmail);
 
-            var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == mloan.LenderEmail);
-
-
+            //sell btc,eth at market price
             if (mloan.coin == "ETH")
             {
-                userWallet.ETH += mloan.amnt;
+                var price = PriceByCoin("ethereum");//usd
+                var rpayCoin = mloan.rPay / price;
+                LoanModel._coinpayUserWallet.ETH += rpayCoin;//selling
+                userWallet.ETH += (mloan.amnt-rpayCoin);
+
             }
             else
             {
-                userWallet.BTC += mloan.amnt;
+                var price = PriceByCoin("bitcoin");
+                var rpayCoin = mloan.rPay / price;
+                LoanModel._coinpayUserWallet.BTC += rpayCoin;//selling
+                userWallet.BTC += (mloan.amnt - rpayCoin);
             }
+
+            LoanModel._coinpayUserWallet.USD -= mloan.rPay;
+            lenerWallet.USD += mloan.rPay;
 
             //operate
             LoanModel._loans.Remove(mloan);
@@ -221,6 +208,69 @@ namespace EthMLM.Controllers
 
 
             return RedirectToAction("Index");
+        }
+        private double PriceByCoin(string coin)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids="+coin);
+                dynamic data = JObject.Parse(json.TrimStart('[').TrimEnd(']'));
+                return (double)data.current_price;
+            }
+        }
+        public IActionResult Coinpay()
+        {
+            
+
+            return View(LoanModel._coinpayUserWallet);
+        }
+        [HttpPost]
+        public IActionResult Coinpay(double iRate,int cRate,int aRate)
+        {
+            LoanModel._dailyInterestRate = iRate / 100;
+            LoanModel._cRate = cRate;
+            LoanModel._allowanceRate = aRate;
+            return View(LoanModel._coinpayUserWallet);
+        }
+        public IActionResult LiquidateUsers()
+        {
+            return View(LoanModel._loans.Where(x => x.status == "Approved").ToList());
+        }
+        [HttpPost]
+        public IActionResult LiquidateUsers(string coin,double price)
+        {
+            //var ratio = 40 / 200;
+            var loans = LoanModel._loans.Where(x => x.coin == coin && x.status == "Approved" && (x.coinPrice - (x.coinPrice * 0.2))>=price).ToList();
+            foreach (var mloan in loans)
+            {
+                //var mloan = LoanModel._loans.FirstOrDefault(x => x.date.ToString().Contains(date.ToString()));
+                var userWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == mloan.Email);
+                var lenerWallet = UserWalletModel._userWallet.FirstOrDefault(x => x.Email == mloan.LenderEmail);
+
+                //sell btc,eth at market price
+                if (mloan.coin == "ETH")
+                {
+                    //var price = PriceByCoin("ethereum");//usd
+                    var rpayCoin = mloan.rPay / price;
+                    LoanModel._coinpayUserWallet.ETH += rpayCoin;//selling
+                    userWallet.ETH += (mloan.amnt - rpayCoin);
+
+                }
+                else
+                {
+                    //var price = PriceByCoin("bitcoin");
+                    var rpayCoin = mloan.rPay / price;
+                    LoanModel._coinpayUserWallet.BTC += rpayCoin;//selling
+                    userWallet.BTC += (mloan.amnt - rpayCoin);
+                }
+
+                LoanModel._coinpayUserWallet.USD -= mloan.rPay;
+                lenerWallet.USD += mloan.rPay;
+
+                //operate
+                LoanModel._loans.Remove(mloan);
+            }
+            return RedirectToAction("LiquidateUsers");
         }
     }
 }
--- EthMLM/Models/LoanModel.cs
+++ EthMLM/Models/LoanModel.cs
@@ -8,13 +8,14 @@ namespace EthMLM.Models
 {
 	public class Loan
 	{
-		public decimal usd{ get; set; }//borrowed usd
+		public double usd{ get; set; }//borrowed usd
 		public string coin { get; set; }
-		public int cRate { get; set; } //collateral rate
-		public decimal amnt { get; set; } //collateral amount
+		public int cRate { get; set; } = LoanModel._cRate; //collateral rate
+		public double amnt { get; set; } //collateral amount=coinPrice*2 //liquidate if price falls 20%
+		public double coinPrice { get; set; }
 		public int days { get; set; }
-		public int iRate { get; set; } //interest rate
-		public decimal rPay { get; set; } //repay usd
+		public double iRate { get; set; }  //interest rate
+		public double rPay { get; set; } //repay usd
 		public DateTime date { get; set; } = DateTime.UtcNow;
 		public string Email { get; set; }
 		public string LenderEmail { get; set; } = "";
@@ -23,9 +24,9 @@ namespace EthMLM.Models
 	public class UserWallet
 	{
 		public string Email { get; set; }
-		public decimal BTC { get; set; } = 1;
-		public decimal ETH { get; set; } = 1;
-		public decimal USD { get; set; } = 1000;
+		public double BTC { get; set; } = 1;
+		public double ETH { get; set; } = 1;
+		public double USD { get; set; } = 1000;
 	}
 	public static class UserWalletModel
 	{
@@ -34,35 +35,10 @@ namespace EthMLM.Models
 		public static class LoanModel
 	{
 		public static List<Loan> _loans= new List<Loan>();
-		public static List<UserTicket> _userTicket = new List<UserTicket>();
-		public static List<UserTicket> _winnerTicketAtEnd = new List<UserTicket>();//
-		public static int _collectedFund =0 ;
-		public static int _collectedFundAtEnd = 0;
-		public static int _totalWinningTicketSoldAtEnd = 0;
-		const int maxTicket = 99;
-
-		private static List<Ticket> InitTickets()
-        {
-			var tickets = new List<Ticket>();
-			for (int i = 1; i <= maxTicket; i++)
-            {
-				tickets.Add(new Ticket { Number = i });
-			}
-			return tickets;
-		}
-
-		public static void Refresh()
-		{
-			_userTicket = new List<UserTicket>();
-			_collectedFundAtEnd = _collectedFund;
-			_collectedFund = 0;
-		}
-
-		public static void LockUnlock(Ticket ticket)//lock on 20%, unlock on 10%  //calling from buyTicket..so its unlock
-		{
-			
-
-		}
-
+		public static UserWallet  _coinpayUserWallet = new UserWallet { Email = "coinpay@coinpay.com",BTC=10,ETH=10,USD=10000};
+		//yearly interest rate=18.25%  , daily=0.05%
+		public static double _dailyInterestRate = 0.0005;
+		public static int _allowanceRate = 25;//user cant take loan more than 25% of the coin
+		public static int _cRate = 40;
 	}
 }
